2aa5fae83d95ca26e8cc11666cd106532bbdefec
Unknown <maximgran@gmail.com>
2018-07-05 14:41:57 +0300

    Auto stash before merge of "master" and "origin/master"

--- FilePack/FileFinder.py
+++ FilePack/FileFinder.py
@@ -1,35 +1,41 @@
 import ReadFile
 import Crypt
 import os
+import platform
 
 
-def find(name, path):
+def find(data):
     """
-    Поиск полного пути до файла.
-    :param name: Имя целевого файла
-    :param path: Коренной путь поиска
-    :return: путь
+        Поиск полного пути до файла.
+        :param name: Имя целевого файла
+        :param path: Коренной путь поиска
+        :return: путь
     """
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
+    root = '/'
+    if platform.system() == 'Windows':
+        root = 'C:\\'
+
+    for root, dirs, files in os.walk(root):
+        for file in files:
+
+            statinfo = os.stat(file)
+            file_size = statinfo.st_size
+            path = os.path.join(root, file)
+
+            for t in data:
+                if t[0][0] == file:
+
+                if t[0][1] == file_size:
+                    if t[0][2]['MD5'] == Crypt.crypt_md5(ReadFile.file_get_contents(file)):
+
+                    if t[0][2]['SHA1'] == Crypt.
 
 
+"""
 def find_all(name, path):
-    """
-    Если требуется найти все совпадения в системе.
-    :param name: Имя целевого файла
-    :param path: Коренной путь поиска
-    :return: путь
-    """
     result = []
     for root, dirs, files in os.walk(path):
         if name in files:
             result.append(os.path.join(root, name))
     return result
-
-
-nameG = input()
-pathG = input()
-md5 = Crypt.crypt_md5(ReadFile.file_get_contents(find(nameG, pathG)))
-print(md5)
+"""
