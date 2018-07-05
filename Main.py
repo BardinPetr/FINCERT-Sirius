b81e92611f5bc06bfb2757b3d942488d9d9448c7
yesmishgan <dolbnin@protonmail.com>
2018-07-05 14:40:44 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+import FilePack as fp
+
+
+def run(data):
+    if 'mail' in data:
+        pass
+    if 'net' in data:
+        pass
+    if 'file' in data:
+        fp.FileFinder.find(data['file'])
+
