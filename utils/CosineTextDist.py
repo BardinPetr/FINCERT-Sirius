854766aada25813e38352c76ddaaef34bbe1c00f
Bardin Petr <bardin.petr@gmail.com>
2018-07-08 12:16:35 +0300

    Cosine text distance added

---
+++ utils/CosineTextDist.py
@@ -0,0 +1,27 @@
+import re, math
+from collections import Counter
+
+WORD = re.compile(r'\w+')
+
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+
+def cosine_dist(a, b):
+    return get_cosine(text_to_vector(a), text_to_vector(b))
