cc57c863ea37561aaacada11a5820cec1a3a5abe
Bardin Petr <bardin.petr@gmail.com>
2018-07-15 15:04:11 +0300

    Minor changes

--- STIX2Parse/Parse.py
+++ STIX2Parse/Parse.py
@@ -1,12 +1,44 @@
 from stix2 import parse
+import re
 
+a = set()
 
-def parse(path):
+def Parse(path):
     indicator = parse(path)
-    print(indicator)
-    res = {}
+    res = {
+        'used': set(),
+        'mail': {
+            'email': [],
+            'text': []
+        },
+        'net': {
+            'ip': [],
+            'url': []
+        },
+        'reg': {
+            'keys': []
+        },
+        'files': [],
+        'ram': {
+            'procs': []
+        },
+        'log': []
+    }
+
+    for obj in indicator.objects:
+        if obj.type == 'indicator':
+            label = re.findall("=(?:\'|\".*\'|\")", obj.labels[0])[0]
+            pattern = obj.pattern
+            print(label, pattern)
+            a.add(label)
+
+    res['used'] = list(res['used'])
     return res
 
 
 if __name__ == '__main__':
-    parse(open("/home/petr/test3/event219.json"))
+    Parse(open("/home/petr/test3/event215.json"))
+    Parse(open("/home/petr/test3/event219.json"))
+    Parse(open("/home/petr/test3/event220.json"))
+    Parse(open("/home/petr/test3/event222.json"))
+    print(a)
