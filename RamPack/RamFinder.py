cc4a4addee46d980a805e5c55139f886223ba7d3
Bardin Petr <bardin.petr@gmail.com>
2018-07-20 15:31:03 +0300

    RamFinder output format fixed

--- RamPack/RamFinder.py
+++ RamPack/RamFinder.py
@@ -2,18 +2,14 @@ import subprocess
 
 
 def find(data, cb):
-    res = []
     processes = data["procs"]
     real_processes = []
 
     for real_proc in [line.split() for line in subprocess.check_output("tasklist").splitlines()]:
-        if (len(real_proc) > 0):
+        if len(real_proc) > 0:
             real_processes.append(str(real_proc[0].decode('cp866')))  # вывод списков
 
-    result = list(set((real_processes)) & set(processes))
+    result = list(set(real_processes) & set(processes))
     for r in result:
         cb("Наличие процесса в системе: {}".format(str(r)))
-    str_result = ", ".join(result, )
-    res.append("Наличие процесса в системе: {}".format(str_result))
-
-    return res
+    return result
